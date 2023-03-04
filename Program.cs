3f044a68a8c56d9c814cce0fbef02c5b1e09a2a9
Cristian Carlesso @kentaromiura <kentaromiura@gmail.com>
2023-03-04 11:52:43 +0900

    Eazy solution

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace ZipJPXtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var filename = args[0];
+            
+            string extractPath = @"extract";
+            extractPath = Path.GetFullPath(extractPath);
+            
+            Encoding shiftJIS = Encoding.GetEncoding("Shift_JIS");
+            ZipFile.ExtractToDirectory(filename, extractPath,shiftJIS);
+            Console.WriteLine("Done! {0}", filename);
+        }
+    }
+}
